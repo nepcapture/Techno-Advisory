22a8344af6c8277c667a838b4345bde81bfc7e14
nepcapture <97738543+nepcapture@users.noreply.github.com>
2026-06-23 09:54:38 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
       parts: [{ text: m.content }]
     }));
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent
 ?key=${GEMINI_API_KEY}`,
       {
         method: 'POST',
